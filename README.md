db63068a3341534e413fc20377567e46cbd78527
John Jorgensen <john@2gn.com>
2025-01-25 08:44:59 -0500

    initial commit of my python learning scaffold

---
+++ README.md
@@ -0,0 +1,5 @@
+# Personal python / data / ml / dl learning flake.
+
+Just a flake for my learning journey.
+
+This flake does not yet support gpu accelerated operations.
---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Python development environment with Jupyter and configurable packages";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        
+        # Customize these packages as needed
+        pythonPackages = ps: with ps; [
+          # Don't remove jupyter. It's pretty core.
+          jupyter
+
+          # packages we'd like to import for our learnings
+          numpy
+          pandas
+          torch
+          torchvision
+          matplotlib
+          scikit-learn
+        ];
+
+        pythonEnv = pkgs.python311.withPackages pythonPackages;
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+           ];
+
+          shellHook = ''
+            echo "Python Jupyter Development Environment"
+            python --version
+            jupyter --version
+            
+            # Optional: Start Jupyter if desired
+            # jupyter notebook
+          '';
+        };
+      }
+    );
+}
